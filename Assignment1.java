36f6fb4d06dbeeb9a2dd7aad3f6962fc7cc487ef
nicsalinetti <salinetti@hartford.edu>
2023-09-18 13:47:00 -0400

    store user entered information

--- Assignment1.java
+++ Assignment1.java
@@ -1,14 +1,30 @@
 // Import libraries here
 // Nicolas Salinetti
 
+import java.util.Scanner;
+
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
+
+    String input;
+    int temp;
+    Scanner scan = new Scanner(System.in);
+
     System.out.println("NNN   NNN    SSSSSSSS");
     System.out.println("NNNN  NNN   SSSS");
     System.out.println("NNNNN NNN    SSSS");
     System.out.println("NNN NNNNN       SSSS");
     System.out.println("NNN  NNNN        SSSS");
     System.out.println("NNN   NNN   SSSSSSSS");
+
+    System.out.println("Please enter a 5-character string:");
+    input = scan.nextLine();
+    System.out.println("Please enter a number in Fahrenheit:");
+    temp = scan.nextInt();
+
+
+
+
   }
 }
