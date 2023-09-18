9eae26ad57c44be1ab3ed88a98c0067eba8fa073
nicsalinetti <salinetti@hartford.edu>
2023-09-18 14:43:52 -0400

    convert the temp to celsius

--- Assignment1.java
+++ Assignment1.java
@@ -2,13 +2,18 @@
 // Nicolas Salinetti
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
 
     String input;
-    int temp;
+    double tempF;
+    double tempC;
+    int randNum;
+
+    Random rand = new Random();
     Scanner scan = new Scanner(System.in);
 
     System.out.println("NNN   NNN    SSSSSSSS");
@@ -21,10 +26,11 @@ public class Assignment1 {
     System.out.println("Please enter a 5-character string:");
     input = scan.nextLine();
     System.out.println("Please enter a number in Fahrenheit:");
-    temp = scan.nextInt();
+    tempF = scan.nextDouble();
+    System.out.println("Random number generated. Continuing...");
 
+    tempC = ((5 * (tempF - 32.0)) / 9);
 
-
-
+    
   }
 }
