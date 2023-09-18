8bc9e0edb2d0ffd81306ee3ecd3fca494f20e27b
nicsalinetti <salinetti@hartford.edu>
2023-09-18 13:37:38 -0400

    print initials in block letters

--- Assignment1.java
+++ Assignment1.java
@@ -4,5 +4,11 @@
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
+    System.out.println("NNN   NNN    SSSSSSSS");
+    System.out.println("NNNN  NNN   SSSS");
+    System.out.println("NNNNN NNN    SSSS");
+    System.out.println("NNN NNNNN       SSSS");
+    System.out.println("NNN  NNNN        SSSS");
+    System.out.println("NNN   NNN   SSSSSSSS");
   }
 }
