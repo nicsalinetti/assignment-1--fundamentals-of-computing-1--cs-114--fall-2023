7e6f4f6a97eb717caeb01b39e31e13220a5286c5
nicsalinetti <salinetti@hartford.edu>
2023-09-20 08:48:40 -0400

    edit printed initials to one print

--- Assignment1.java
+++ Assignment1.java
@@ -1,11 +1,10 @@
-// Import libraries here
 // Nicolas Salinetti
 
 import java.util.Scanner;
 
 public class Assignment1 {
   public static void main(String[] args) {
-    
+
     String input;
     double tempF;
     double tempC;
@@ -13,12 +12,14 @@ public class Assignment1 {
 
     Scanner scan = new Scanner(System.in);
 
-    System.out.println("NNN   NNN    SSSSSSSS");
-    System.out.println("NNNN  NNN   SSSS");
-    System.out.println("NNNNN NNN    SSSS");
-    System.out.println("NNN NNNNN       SSSS");
-    System.out.println("NNN  NNNN        SSSS");
-    System.out.println("NNN   NNN   SSSSSSSS");
+    System.out.println(
+      "NNN   NNN    SSSSSSSS"
+    + "\nNNNN  NNN   SSSS"
+    + "\nNNNNN NNN    SSSS"
+    + "\nNNN NNNNN       SSSS"
+    + "\nNNN  NNNN        SSSS"
+    + "\nNNN   NNN   SSSSSSSS"
+    );
 
     System.out.println("Please enter a 5-character string:");
     input = scan.nextLine();
@@ -29,7 +30,7 @@ public class Assignment1 {
     tempC = ((5 * (tempF - 32.0)) / 9);
 
     randNum = (int)(Math.random() * 16384) + 32;
-    
+
     String letter2 = input.substring(1,2);
     String letter3 = input.substring(2,3);
     String letter4 = input.substring(3,4);
