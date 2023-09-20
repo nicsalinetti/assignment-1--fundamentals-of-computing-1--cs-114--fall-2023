87bb725ace6cabbdb7e4ac7db3cd59649dec066a
nicsalinetti <salinetti@hartford.edu>
2023-09-20 09:19:56 -0400

    force the user to input a 5 letter string

--- Assignment1.java
+++ Assignment1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Assignment1 {
   public static void main(String[] args) {
 
-    String input;
+    String input = "x";
     double tempF;
     double tempC;
     int randNum;
@@ -21,8 +21,18 @@ public class Assignment1 {
       + "\nNNN   NNN   SSSSSSSS"
     );
 
-    System.out.println("Please enter a 5-character string:");
-    input = scan.nextLine();
+    for(int i = 1; i == 1;){
+      System.out.println("Please enter a 5-character string:");
+      input = scan.nextLine();
+      int length = input.length();
+      if(length > 5 || length < 5){
+        i = 1;
+      }
+      else{
+        i = 0;
+      }
+    }
+
     System.out.println("Please enter a number in Fahrenheit:");
     tempF = scan.nextDouble();
     System.out.println("Random number generated. Continuing...");
